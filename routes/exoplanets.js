fbe15567f86825838703c7fdbb69b6525edae9c1
touzani-marwa-vinci <marwa.touzani@student.vinci.be>
2024-10-10 17:03:32 +0200

    cours chiant

--- routes/exoplanets.js
+++ routes/exoplanets.js
@@ -29,7 +29,7 @@ router.get('/', function (req, res, next) {
 
 /* POST add exoplanet. */
 router.post('/add', upload.single('imageExoplanet'), function (req, res, next) {
-    console.log("POST ADD EXOPLANET");
+    console.log("POST ADD EXOPLANETs");
     // validate name of explanet -> betweeen 3 and 100 character
     if (validator.isLength(req.body.uniqueNameExoplanet, { min: 3, max: 100 })) {
         console.log("req.file : " + JSON.stringify(req.file));
